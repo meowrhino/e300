99d10515b4eb48740e1c2c99691bd86df49c8e01
manu <manuellatourf@gmail.com>
2024-10-17 11:30:39 +0200

    updated todo menos los proyectos

--- script/styleLinks.js
+++ script/styleLinks.js
@@ -6,6 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (link.hasAttribute('href') && link.getAttribute('href') !== '') {
             // Si tiene un href, asignar la clase 'link-con-href'
             link.classList.add('link-con-href');
+
+            // Hacer que el enlace se abra en una nueva pestaña solo si no es un link de menu
+            if (!link.closest('.language-links') && !link.closest('.menu-links')) {
+                link.setAttribute('target', '_blank');
+            }
         } else {
             // Si no tiene href o está vacío, asignar la clase 'link-sin-href'
             //link.classList.add('link-sin-href');
